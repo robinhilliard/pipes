0eabdbb4f982fece7f558eda742fac8b088db526
Robin Hilliard <robin@rocketboots.com>
2018-04-09 23:54:09 +1000

    fixing dist version and url

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='pipeop',
   packages=['pipeop'],
-  version='0.1.0',
+  version='0.1.1',
   description='A decorator that changes the >> operator to mimic Elixir-style function pipes',
   author='Robin Hilliard',
   author_email='robin@rocketboots.com',
   url='https://github.com/robinhilliard/pipes',
-  download_url='https://github.com/robinhilliard/pipes/dist/pipeop-0.1.0.tar.gz',
+  download_url='https://github.com/robinhilliard/pipes/dist/pipeop-0.1.1.tar.gz',
   keywords=['python', 'elixir', 'pipe'],
   classifiers=[],
 )
