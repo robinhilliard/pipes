21763c80e8ee0200d453b722c39a9c075716762d
Robin Hilliard <robin@rocketboots.com>
2018-04-09 23:57:37 +1000

    0.1.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='pipeop',
   packages=['pipeop'],
-  version='0.1.1',
+  version='0.1.2',
   description='A decorator that changes the >> operator to mimic Elixir-style function pipes',
   author='Robin Hilliard',
   author_email='robin@rocketboots.com',
   url='https://github.com/robinhilliard/pipes',
-  download_url='https://github.com/robinhilliard/pipes/dist/pipeop-0.1.1.tar.gz',
+  download_url='https://github.com/robinhilliard/pipes/blob/master/dist/pipeop-0.1.2.tar.gz',
   keywords=['python', 'elixir', 'pipe'],
   classifiers=[],
 )
