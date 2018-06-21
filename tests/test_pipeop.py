402f700ac9a14b86af2e1b0104a467eb4d97b7ac
Robin Hilliard <robin@rocketboots.com>
2018-06-21 21:56:29 +1000

    Add basic tests

---
+++ tests/test_pipeop.py
@@ -0,0 +1,25 @@
+import unittest
+from pipeop import pipes
+
+
+def add2(a, b):
+    return a + b
+
+
+def add3(a, b, c):
+    return a + b + c
+
+
+class PipeOpTestCase(unittest.TestCase):
+
+    @pipes
+    def test_pipe_one_arg(self):
+        assert [1, 2, 3] >> sum() == 6
+
+    @pipes
+    def test_pipe_two_args(self):
+        assert 1 >> add2(2) == 3
+
+    @pipes
+    def test_pipe_three_args(self):
+        assert 1 >> add3(2, 3) == 6
