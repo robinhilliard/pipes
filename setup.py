1716d5a5cdfa6cae72d8d56abd205d6dde08b7d1
Robin Hilliard <robin@rocketboots.com>
2018-06-23 11:18:24 +1000

    Separate long_description into README.rst

--- setup.py
+++ setup.py
@@ -2,30 +2,17 @@ from distutils.core import setup
 
 version = '0.1.7'
 
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
 setup(
   name='pipeop',
   packages=['pipeop'],
   version=version,
-  description='A decorator that changes the >> operator to mimic Elixir-style function pipes',
-  long_description=
-  """
-  Within the decorated function ``a >> b(...)`` becomes ``b(a, ...)``:
- 
-  ::
-  
-      from pipeop import pipes
-    
-      def add3(a, b, c):
-          return a + b + c
-      
-      def times(a, b):
-          return a * b
-      
-      @pipes
-      def calc()
-          print 1 >> add3(2, 3) >> times(3)  # prints 18
-
-  """,
+  description="""
+  A decorator that changes the >> operator 
+  to mimic Elixir-style function pipes""",
+  long_description=long_description,
   long_description_content_type='text/x-rst',
   author='Robin Hilliard',
   author_email='robin@rocketboots.com',
