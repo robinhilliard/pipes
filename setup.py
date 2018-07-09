93c841839a7cbf7299a3d610158fc072795ed215
Robin Hilliard <robin@rocketboots.com>
2018-07-09 11:21:28 +1000

    0.2.3 Include README.rst in package to prevent failing pip download

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from distutils.core import setup
 
-version = '0.2.2'
 
-with open('README.rst', 'r') as f:
-    long_description = f.read()
+version = '0.2.3'
+
 
 setup(
-  name='pipeop',
-  packages=['pipeop'],
-  version=version,
-  description="A decorator that changes the >> and << operators to mimic Elixir-style function pipes",
-  long_description=long_description,
-  long_description_content_type='text/x-rst',
-  author='Robin Hilliard',
-  author_email='robin@rocketboots.com',
-  license='MIT',
-  url='https://github.com/robinhilliard/pipes',
-  download_url='https://github.com/robinhilliard/pipes/blob/master/dist/pipeop-{}.tar.gz?raw=true'.format(version),
-  keywords='python elixir pipe',
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.6'
-  ],
+    name='pipeop',
+    packages=['pipeop'],
+    version=version,
+    description="A decorator that changes the >> and << operators to mimic Elixir-style function pipes",
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    author='Robin Hilliard',
+    author_email='robin@rocketboots.com',
+    license='MIT',
+    url='https://github.com/robinhilliard/pipes',
+    download_url='https://github.com/robinhilliard/pipes/blob/master/dist/pipeop-{}.tar.gz?raw=true'.format(version),
+    keywords='python elixir pipe',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ],
 )
