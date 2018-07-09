f925128832c1929a1d4c11caef3dfefb20e280f1
Robin Hilliard <robin@rocketboots.com>
2018-07-09 21:59:51 +1000

    Correct line number, column offset, file name and don't change function name so that debug attributes work properly. Remove pipes argument because I now realise that we don't need to check to recompile, because (of course) the decorator runs when the module is loaded, NOT when the function is called or the class instanciated.

--- pipeop/__init__.py
+++ pipeop/__init__.py
@@ -5,8 +5,9 @@
 
 
 from ast import Call, parse, Name, NodeTransformer, LShift, RShift, \
-    increment_lineno
+    increment_lineno, walk
 from inspect import getsource, isclass, stack
+from itertools import takewhile
 from textwrap import dedent
 
 
@@ -36,70 +37,54 @@ class _PipeTransformer(NodeTransformer):
             return node
 
 
-def pipes(func_or_cache_flag=True):
-
-    def pipes_decorator(func_or_class):
-        if isclass(func_or_class):
-            decorated_name = '__pipes_class_{}'.format(
-                func_or_class.__name__)
-
-            decorator_frame = stack()[2]
-            ctx = decorator_frame[0].f_locals
-            first_line_number = decorator_frame[2]
-        else:
-            decorated_name = '__pipes_{}'.format(
-                func_or_class.__code__.co_name)
-
-            ctx = func_or_class.__globals__
-            first_line_number = func_or_class.__code__.co_firstlineno
-
-        # Check to see the decorated function isn't already created
-        # It shouldn't be, but it doesn't hurt to check
-        if not func_or_cache_flag or decorated_name not in ctx:
-            # AST data structure representing parsed function code
-            tree = parse(dedent(getsource(func_or_class)))
-
-            # Fix line numbers so that debuggers still work
-            increment_lineno(tree, first_line_number - 1)
-
-            # Update name of function or class to compile
-            tree.body[0].name = decorated_name
-
-            # remove the pipe decorator so that we don't recursively
-            # call it again. The AST node for the decorator will be a
-            # Call if it had braces, and a Name if it had no braces.
-            # The location of the decorator function name in these
-            # nodes is slightly different.
-            tree.body[0].decorator_list = \
-                [d for d in tree.body[0].decorator_list
-                 if isinstance(d, Call) and d.func.id != 'pipes'
-                 or isinstance(d, Name) and d.id != 'pipes']
-
-            # Apply the visit_BinOp transformation
-            tree = _PipeTransformer().visit(tree)
-
-            # now compile the AST into an altered function or class definition
-            code = compile(
-                tree,
-                filename=__file__,
-                mode="exec")
-
-            # and execute the definition in the original context so that the
-            # decorated function can access the same scopes as the original
-            exec(code, ctx)
-
-        # return the modified function or class - original is never called
-        return ctx[decorated_name]
-
-    if callable(func_or_cache_flag):
-        # No arguments passed to @pipes so we received the function or class to
-        # wrap. Pass function or class to the decorator to check for recompile
-        # and return modified function
-        return pipes_decorator(func_or_cache_flag)
+def pipes(func_or_class):
+    if isclass(func_or_class):
+        decorator_frame = stack()[1]
+        ctx = decorator_frame[0].f_locals
+        first_line_number = decorator_frame[2]
 
     else:
-        # Cache argument was passed to @pipes, so return decorator function
-        # reference. pipes_decorator was defined in a closure with func_or
-        # cache flag being used as a cache flag set to True or False, so it can
-        # access the flag and still be passed the function when it is called.
-        return pipes_decorator
+        ctx = func_or_class.__globals__
+        first_line_number = func_or_class.__code__.co_firstlineno
+
+    source = getsource(func_or_class)
+
+    # AST data structure representing parsed function code
+    tree = parse(dedent(source))
+
+    # Fix line and column numbers so that debuggers still work
+    increment_lineno(tree, first_line_number - 1)
+    source_indent = sum([1 for _ in takewhile(str.isspace, source)]) + 1
+
+    for node in walk(tree):
+        if hasattr(node, "col_offset"):
+            node.col_offset += source_indent
+
+    # Update name of function or class to compile
+    #tree.body[0].name = decorated_name
+
+    # remove the pipe decorator so that we don't recursively
+    # call it again. The AST node for the decorator will be a
+    # Call if it had braces, and a Name if it had no braces.
+    # The location of the decorator function name in these
+    # nodes is slightly different.
+    tree.body[0].decorator_list = \
+        [d for d in tree.body[0].decorator_list
+         if isinstance(d, Call) and d.func.id != 'pipes'
+         or isinstance(d, Name) and d.id != 'pipes']
+
+    # Apply the visit_BinOp transformation
+    tree = _PipeTransformer().visit(tree)
+
+    # now compile the AST into an altered function or class definition
+    code = compile(
+        tree,
+        filename=ctx['__file__'],
+        mode="exec")
+
+    # and execute the definition in the original context so that the
+    # decorated function can access the same scopes as the original
+    exec(code, ctx)
+
+    # return the modified function or class - original is never called
+    return ctx[tree.body[0].name]
